8e94c39aed48df947e1a593c48c8d5e928e4afe6
colyerdeng <colyerdeng@tencent.com>
2026-03-08 23:32:51 +0800

    feat: add client

---
+++ src/types/client-error.ts
@@ -0,0 +1,31 @@
+export type ShotGridClientErrorKind = 'auth_failed' | 'http_error' | 'unexpected'
+
+export class ShotGridClientError extends Error {
+	readonly kind: ShotGridClientErrorKind
+	readonly status?: number
+	readonly code?: number
+	readonly title?: string
+	readonly detail?: string
+	readonly cause?: unknown
+
+	constructor(
+		message: string,
+		options: {
+			kind: ShotGridClientErrorKind
+			status?: number
+			code?: number
+			title?: string
+			detail?: string
+			cause?: unknown
+		},
+	) {
+		super(message)
+		this.name = 'ShotGridClientError'
+		this.kind = options.kind
+		this.status = options.status
+		this.code = options.code
+		this.title = options.title
+		this.detail = options.detail
+		this.cause = options.cause
+	}
+}
---
+++ src/types/json-api.ts
@@ -0,0 +1,10 @@
+export interface JsonApiRecord<TAttributes extends object = object> {
+	type: string
+	id: string
+	attributes: TAttributes
+}
+
+export interface JsonApiResponse<TData> {
+	data: TData
+	links: { self: string; next?: string; prev?: string }
+}
