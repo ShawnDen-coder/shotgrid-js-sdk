7547306b4df0ff41502a19d00bd172ef1130e43d
colyerdeng <colyerdeng@tencent.com>
2026-03-09 20:10:15 +0800

    feat: add github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  publicDir: false,
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'ShotGridSDK',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format === 'es' ? 'mjs' : 'cjs'}`,
+    },
+    rollupOptions: {
+      external: ['axios', 'tslog'],
+    },
+  },
+  plugins: [dts({ rollupTypes: true })],
   resolve: {
-    alias: [
-      {
-        find: /^@(.+)$/,
-        replacement: new URL('./src/$1', import.meta.url).pathname,
-      },
-    ],
+    alias: [{ find: /^@(.+)$/, replacement: new URL('./src/$1', import.meta.url).pathname }],
   },
 })
